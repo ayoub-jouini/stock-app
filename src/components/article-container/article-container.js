454df064174f1272ed4ea9df32a7a258a0e71438
ayoub_jouini <ayoubjouini135@gmail.com>
2021-07-10 16:25:21 +0100

    fetch Data & useEffect

--- src/components/article-container/article-container.js
+++ src/components/article-container/article-container.js
@@ -1,25 +1,42 @@
 import './article-container.css';
 import Article from '../article/article';
 import AddBtn from '../add-btn/add-btn';
-import Data from '../../Data.json';
+import React, { useEffect, useState } from 'react';
 
 
 const ArticleContrainer = ({ artData }) => {
+    const [data, setData] = useState([]);
+    async function getData() {
+        const response = await fetch('Data.json'
+            , {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        )
+        if (response.ok) return response.json();
+        throw response;
+
+    }
+    useEffect(() => {
+        getData().then((response) => setData(response));
+    }, [])
     return (
         <div className="art-cont">
-            {Data.filter((data) => {
+            {data && data.length > 0 && data.filter((d) => {
                 if (artData === '') {
-                    return data
-                } else if ((data.name.toLowerCase().includes(artData.toLowerCase())) || (data.code.includes(artData))) {
-                    return data
+                    return d
+                } else if ((d.name.toLowerCase().includes(artData.toLowerCase())) || (d.code.includes(artData))) {
+                    return d
                 }
-            }).map(data =>
+            }).map(d =>
                 <Article
-                    key={data.id}
-                    name={data.name}
-                    code={data.code}
-                    qte={data.qte}
-                    zone={data.zone} />
+                    key={d.id}
+                    name={d.name}
+                    code={d.code}
+                    qte={d.qte}
+                    zone={d.zone} />
             )}
 
             <AddBtn />
