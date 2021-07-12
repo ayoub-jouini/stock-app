06f723842a25a200ed10272dc503f1605d5fabe4
ayoub_jouini <ayoubjouini135@gmail.com>
2021-07-12 14:20:53 +0100

    details page (details-body)

---
+++ src/components/details-body/detailsBody.js
@@ -0,0 +1,34 @@
+import './detailsBody.css';
+
+const DetailsBody = ({ name, code, qte, zone }) => {
+    return (
+        <>
+            <div className="header-div">
+                <div className="left-icons">
+                    <div>
+                        <img alt="plus-btn" src="/asset/plus.svg" />
+                    </div>
+                    <div>
+                        <img alt="moins-btn" src="/asset/moins.svg" />
+                    </div>
+                </div>
+                <div className="name">
+                    {name}
+                </div>
+                <div className="edit-icon">
+                    <img alt="edit-btn" src="/asset/edit.svg" />
+                </div>
+            </div>
+            <div className="body-div">
+                <ul>
+                    <li><div>Code:</div><div>{code}</div></li>
+                    <li><div>Quantités:</div><div>{qte}</div></li>
+                    <li><div>Zone:</div><div>{zone}</div></li>
+                    <li><div>Description:</div><div>...</div></li>
+                </ul>
+            </div>
+        </>
+    );
+}
+
+export default DetailsBody;
